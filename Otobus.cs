cde273873d7fc2551b0a4a7ccb186f58f832cea8
Zeynep ÇELİK <zeynep-9967@hotmail.com>
2023-12-26 00:35:56 +0300

    OtobusBileti

---
+++ Otobus.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtobusBileti
+{
+    public class Otobus
+    {
+        public string FirmaAdi;
+        public int ToplamKoltukSayisi;
+        public int BosKoltukSayisi;
+        public string Gun;
+        public string Saat;
+        public string Plaka;
+        public string Guzergah;
+        public string MolaYeri;
+        public int Indirimli;
+        public int Tam;
+        public int Kazanc = 0;
+        public int IptalSayisi = 0;
+
+
+        public Otobus(string FirmaAdi, int KoltukSayisi, string Gun, string Saat, string Plaka, 
+            string Guzergah, string MolaYeri, int Indirimli, int Tam)
+        {
+            this.FirmaAdi = FirmaAdi;
+            this.ToplamKoltukSayisi = KoltukSayisi;
+            this.BosKoltukSayisi = KoltukSayisi;
+            this.Gun = Gun;
+            this.Saat = Saat;
+            this.Plaka = Plaka;
+            this.Guzergah = Guzergah;
+            this.MolaYeri = MolaYeri;
+            this.Indirimli = Indirimli;
+            this.Tam = Tam;
+
+        }
+
+        
+        public void BiletSat(bool indirimliMi)
+        {
+            if (BosKoltukSayisi > 0 )
+            {
+                BosKoltukSayisi--;
+
+                if (indirimliMi) // İndirimli bilet satışı
+                {
+                    Kazanc = Kazanc + Indirimli;
+                    Console.WriteLine("Bir adet indirimli bilet satıldı.");
+                }
+                else // İndirimsiz bilet satışı
+                {
+                    Kazanc = Kazanc + Tam;
+                    Console.WriteLine("Bir adet tam bilet satıldı.");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Boş koltuk yoktur.");
+            }
+        }
+
+
+        public void BiletIptal(bool indirimliMi)
+        {
+            IptalSayisi++;
+            BosKoltukSayisi++;
+            if (indirimliMi)
+            {
+                Kazanc = Kazanc - Indirimli;
+            }
+            else
+            {
+                Kazanc = Kazanc - Tam;
+            }
+
+        }
+
+
+        public void BosKoltukOgren()
+        {
+            Console.WriteLine("Toplam satılan bilet sayısı:" + (ToplamKoltukSayisi - BosKoltukSayisi));
+            Console.WriteLine("Boş koltuk sayısı: " + BosKoltukSayisi);
+
+        }
+
+
+        public void KazancOgren()
+        {
+            Console.WriteLine("Kazanç: " + Kazanc);
+
+        }
+
+        public void IptalSayisiOgren()
+        {
+            Console.WriteLine("İptal edilen bilet sayısı: " + IptalSayisi);
+        }
+       
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtobusBileti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Otobus otobus = new Otobus("Metro", 40, "Pazartesi", "15:00", "34 ABC 123", "Düzce - İstanbul", "Berceste Dinlenme Tesisleri", 400, 600);
+
+            Console.WriteLine("Otobüs Firması: " + otobus.FirmaAdi);
+            Console.WriteLine("Otobüs Güzergahı: " + otobus.Guzergah);
+            Console.WriteLine("Otobüs Kalkış Saati: " + otobus.Saat);
+            Console.WriteLine("Mola Yeri: " + otobus.MolaYeri);
+
+            otobus.BiletSat(true);
+            otobus.BiletSat(true);
+            otobus.BiletSat(false);
+            otobus.BiletSat(true);
+            otobus.BiletSat(false);
+            otobus.BiletSat(false);
+            otobus.BiletSat(true);
+
+            otobus.BosKoltukOgren();
+
+            otobus.KazancOgren();
+
+            otobus.MolaYeri = "Türsan Dinlenme Tesisleri"; // Mola yeri değiştirdik
+            Console.WriteLine("Mola Yeri: " + otobus.MolaYeri);
+
+            otobus.BiletIptal(true);
+            otobus.BiletIptal(false);
+            otobus.BiletIptal(true);
+
+            otobus.IptalSayisiOgren(); 
+
+            otobus.BosKoltukOgren();
+
+            otobus.KazancOgren();
+
+
+        }
+    }
+}
